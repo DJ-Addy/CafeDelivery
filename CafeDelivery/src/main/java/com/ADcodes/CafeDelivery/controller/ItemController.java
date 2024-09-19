473428a313ec865e7201f53d67cc5b937dc17d62
DJ-Addy <achermouh808@gmail.com>
2024-09-18 23:33:46 -0500

    sdfsd

--- CafeDelivery/src/main/java/com/ADcodes/CafeDelivery/controller/ItemController.java
+++ CafeDelivery/src/main/java/com/ADcodes/CafeDelivery/controller/ItemController.java
@@ -3,6 +3,8 @@ package com.ADcodes.CafeDelivery.controller;
 
 import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ADcodes.CafeDelivery.model.Items;
@@ -18,16 +19,21 @@ import com.ADcodes.CafeDelivery.repo.ItemRepo;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
-@RequestMapping("/items")
 public class ItemController {
 
     @Autowired
     private ItemRepo itemRepo;
 
     // Add new item
-    @PostMapping("/items")
-    public Items addItem(@RequestBody Items newItem) {
-        return itemRepo.save(newItem);
+   @PostMapping("/items")
+    public ResponseEntity<Items> addItem(@RequestBody Items newItem) {
+        try {
+            Items savedItem = itemRepo.save(newItem);
+            return ResponseEntity.ok(savedItem);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     // Update existing item
@@ -68,13 +74,13 @@ public class ItemController {
     }
 
     // Get items by name
-    @GetMapping("/name/{itemName}")
+    @GetMapping("/items/name/{itemName}")
     public List<Items> getItemsByName(@PathVariable String itemName) {
         return itemRepo.findByItemName(itemName);
     }
 
     // Get items by category
-    @GetMapping("/category/{itemCat}")
+    @GetMapping("/items/category/{itemCat}")
     public List<Items> getItemsByCategory(@PathVariable String itemCate) {
         return itemRepo.findByItemCate(itemCate);
     }
